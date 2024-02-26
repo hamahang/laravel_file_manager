66ae5946dd81019da8223a4285e04de93471d370
faramarz <faramarz.sadeghi89@gmail.com>
2024-02-26 14:38:15 +0330

    debug

--- src/Requests/DirectUploadFile.php
+++ src/Requests/DirectUploadFile.php
@@ -5,6 +5,7 @@ namespace Hamahang\LFM\Requests;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Http\Exceptions\HttpResponseException;
+use Nette\Schema\ValidationException;
 
 class DirectUploadFile extends FormRequest
 {
@@ -22,6 +23,7 @@ class DirectUploadFile extends FormRequest
         $options = $LFM['options'];
         $this->options = $options;
         $this->file_ratio = isset($options['ratio']) ? $options['ratio'] : NULL;
+        $this->dimensions = isset($options['dimensions']) ? $options['dimensions'] : NULL;
 
         return true;
     }
@@ -35,7 +37,7 @@ class DirectUploadFile extends FormRequest
     public function rules()
     {
         $roles = [
-            'file'    => 'required|lfm_check_size|lfm_check_true_mime_type|lfm_check_img_dimensions',
+            'file'    => 'required|lfm_check_size|lfm_check_true_mime_type|lfm_check_img_ratio|lfm_check_img_dimensions',
             'section' => 'required'
         ];
 
@@ -45,16 +47,19 @@ class DirectUploadFile extends FormRequest
     protected function failedValidation(Validator $validator)
     {
         $api_errors = [];
+        $fails = [];
         $errors = $validator->errors();
         foreach ($errors->getMessages() as $values)
         {
             foreach ($values as $key => $value)
             {
                 $api_errors[ $key ] = $value;
+                $fails[] = $validator->failed() ;
             }
         }
         $res =
             [
+                'fails'            => $fails,
                 'errors'            => $api_errors,
                 'success'           => false,
                 'pre_required_erro' => true,
@@ -86,7 +91,7 @@ class DirectUploadFile extends FormRequest
             return true;
         });
 
-        $validator->addImplicitExtension('lfm_check_img_dimensions', function ($attribute, $values, $parameters) use ($options) {
+        $validator->addImplicitExtension('lfm_check_img_ratio', function ($attribute, $values, $parameters) use ($options) {
             if ($this->file_ratio)
             {
                 foreach ($values as $file)
@@ -97,7 +102,7 @@ class DirectUploadFile extends FormRequest
                         $width = $image_info[0];
                         $height = $image_info[1];
                         $ratio = $width / $height;
-                        if ($ratio != $this->file_ratio)
+                        if (round($ratio , 1) != round($this->file_ratio,1))
                         {
                             return false;
                         }
@@ -106,6 +111,40 @@ class DirectUploadFile extends FormRequest
             }
 
             return true;
+        });
+
+
+        $validator->addImplicitExtension('lfm_check_img_dimensions', function ($attribute, $values, $parameters) use ($options) {
+            $dimensions = $this->dimensions ;
+            $check = true ;
+            if ($dimensions && is_array($dimensions) && count($dimensions) == 2)
+            {
+
+                $minDim = $dimensions [0] ;
+                $maxDim = $dimensions [1] ;
+                $minDim = explode('*',$minDim) ;
+                $maxDim = explode('*',$maxDim) ;
+                if (count($minDim) == 2 && count($maxDim) == 2)
+                {
+                    foreach ($values as $file)
+                    {
+                        $image_info = getimagesize($file);
+                        if ($image_info && isset($image_info[0]) && isset($image_info[1]) && $image_info[1] != 0)
+                        {
+                            $width = $image_info[0];
+                            $height = $image_info[1];
+//                            dd($width,$height,$minDim[0],$minDim[1],$maxDim[0],$maxDim[1]);
+                            if ($width < $minDim[0] || $height < $minDim[1] || $width > $maxDim[0] || $height > $maxDim[1]  )
+                            {
+                                $check = false ;
+                            }
+
+                        }
+                    }
+                }
+            }
+
+            return $check;
         });
 
         $validator->addImplicitExtension('lfm_check_true_mime_type', function ($attribute, $values, $parameters) use ($options) {
@@ -134,6 +173,7 @@ class DirectUploadFile extends FormRequest
             'file.required'                 => 'آپلود فایل ضروری است .',
             'file.lfm_check_size'           => 'خطا در سایز فایل .',
             'file.lfm_check_true_mime_type' => 'خطا در نوع فایل آپلود شده .',
+            'file.lfm_check_img_ratio' => 'تصویر دارای ابعاد نامناسب است(ابعاد قابل قبول ' . $ratio . ')',
             'file.lfm_check_img_dimensions' => 'تصویر دارای ابعاد نامناسب است(ابعاد قابل قبول ' . $ratio . ')',
         ];
 
